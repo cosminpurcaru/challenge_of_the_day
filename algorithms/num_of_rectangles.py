2af9ab05a01003b5d6b792f48508e84148083977
Cosmin <61462099+cosminpurcaru@users.noreply.github.com>
2021-03-02 21:13:32 +0000

    Add problem description

--- algorithms/num_of_rectangles.py
+++ algorithms/num_of_rectangles.py
@@ -1,3 +1,25 @@
+"""
+Given a list of points represented as N(x,y)), find the maximum number of rectangles 
+that can be formed using these points.
+
+Note:
+Rectangles are formed only so that their sides are parallel against Ox & Oy.
+
+Example:
+
+    Input:
+
+  y ^
+    |            *
+    |    *   *          *
+    |    *   *   *      *
+----|-------------------------> x
+   O|
+
+    Output:
+    3
+"""
+
 class Point:
 
     def __init__(self, x, y):
@@ -6,7 +28,13 @@ class Point:
 
 
 def get_num_of_rectangles(points: list):
+    """
+    Get maximum number of rectangles that can be formed from a list of points.
+    
+    :param points: List of Points represented as P(x, y)
+    """
     verticals = {}
+
     for point in points:
         for point_above in points:
             if (point.x == point_above.x) and (point.y < point_above.y):
