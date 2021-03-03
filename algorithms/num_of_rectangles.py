6145295ed3ace347edb06c874e92f04710540acc
Cosmin <61462099+cosminpurcaru@users.noreply.github.com>
2021-03-03 10:56:02 +0000

    Update num_of_rectangles.py

--- algorithms/num_of_rectangles.py
+++ algorithms/num_of_rectangles.py
@@ -34,18 +34,19 @@ def get_num_of_rectangles(points: list):
     """
     Get maximum number of rectangles that can be formed from a list of points.
     
-    :param points: List of Points represented as P(x, y)
+    :param points: List of Points represented as Point(x, y)
+    :return: Highest number of rectangles whose sides are parallel with Ox & Oy
     """
     verticals = {}
 
     for point in points:
         for point_above in points:
             if (point.x == point_above.x) and (point.y < point_above.y):
-                y_diff = point_above.y - point.y
-                if (y_diff) in verticals:
-                    verticals[y_diff] += 1
+                vertical = (point.y, point_above.y)
+                if vertical in verticals:
+                    verticals[vertical] += 1
                 else:
-                    verticals[y_diff] = 1
+                    verticals[vertical] = 1
 
     print(f'Verticals: {verticals}')
     rectangles = [int((vertical*(vertical-1)/2)) for vertical in list(verticals.values())]
