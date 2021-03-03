4fa3a2529d79401fb91ef53b9c8fef44a618be0d
Cosmin <61462099+cosminpurcaru@users.noreply.github.com>
2021-03-03 10:06:55 +0000

    Create point.py

---
+++ lib/point.py
@@ -0,0 +1,14 @@
+
+
+class Point:
+    """Class for representing a Point in Oxy system.""" 
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__():
+      return f'Point coordinates: x={self.x}; y={self.y}'
+    
+    def __repr__():
+      return f'Point('x={self.x}; y={self.y})'
